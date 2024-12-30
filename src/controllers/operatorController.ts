bf2b2d5eb1c179956e9b2eb589afd6cd0925ddec
BenjaminAguirre <benjamin@ongrid.run>
2024-12-30 11:50:03 -0300

    Update

--- src/controllers/operatorController.ts
+++ src/controllers/operatorController.ts
@@ -5,31 +5,60 @@ import {
     generateExternalIdentityKeypair,
     generateAddressKeypair
 } from "../lib/flux/wallet";
-import {generateAkashKeypair} from "../lib/akash/walletA"
+import { generateAkashKeypair } from "../lib/akash/walletA"
+
+
+
+const createGridAccount = async (_req: any, res: any) => {
+    try {
+        const username = _req.body.username;
+        const password = _req.body.password;
+        const phrase = username + password;
+        const MasterKeys = await generatexPubxPriv(phrase, 44, 118, 0, "p2sh")
+        const xpriv = MasterKeys.xpriv;
+        const FluxId = await generateExternalIdentityKeypair(xpriv);
+        const FluxKeys = await generateAddressKeypair(xpriv, 0, 118)
+        const AkashKeys = await generateAkashKeypair(xpriv);
+        const user = {
+            xpriv,
+            FluxData:
+            {
+                FluxId,
+                FluxKeys
+            },
+            AkashKeys,
+        }
+
+        res.status(200).send(user)
+
+
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
 
 
 
 
-
-
-const fluxTest = async(_req: any, res: any) =>{
+const fluxTest = async (_req: any, res: any) => {
     try {
         const username = _req.body.username;
         const password = _req.body.password;
         const phrase = username + password;
 
         // m / 44' / 118' / 0' / 0 / address_index
-         // m / 44' / 19167' / 0' / 0 / address_index
+        // m / 44' / 19167' / 0' / 0 / address_index
 
 
-        const response = await generatexPubxPriv(phrase, 44, 118,0 ,"p2sh")
+        const response = await generatexPubxPriv(phrase, 44, 118, 0, "p2sh")
         const xpriv = response.xpriv;
         // const akashData =generateAkashKeypair(xpriv)
-        const fluxData = await generateAddressKeypair(xpriv,0,118)
+        const fluxData = await generateAddressKeypair(xpriv, 0, 118)
 
         const accountData = {
             response,
             fluxData,
+            xpriv
             // akashData
         }
         res.status(200).send(accountData)
@@ -40,25 +69,25 @@ const fluxTest = async(_req: any, res: any) =>{
     }
 }
 
-const akashAccount = async(_req:any, res:any)=>{
+const akashAccount = async (_req: any, res: any) => {
     try {
-        const xprivKey = _req.body.xpriv 
-        const response = generateAkashKeypair(xprivKey)
+        const xprivKey = _req.body.xpriv;
+        const response = await generateAkashKeypair(xprivKey);
         res.status(200).send(response)
     } catch (error) {
         res.status(500).send(error);
     }
 }
 
-const XprivXpub = async(_req: any, res: any) =>{
+const XprivXpub = async (_req: any, res: any) => {
     try {
         const username = _req.body.username;
         const password = _req.body.password;
         const phrase = username + password;
 
         // m / 44' / 118' / 0' / 0 / address_index
-        const response = await generatexPubxPriv(phrase, 44, 118,0 ,"p2sh")
-        
+        const response = await generatexPubxPriv(phrase, 44, 118, 0, "p2sh")
+
         res.status(200).send(response)
 
 
@@ -68,13 +97,13 @@ const XprivXpub = async(_req: any, res: any) =>{
 }
 
 
-const ChildKeypair = async(_req: any, res: any) =>{
+const ChildKeypair = async (_req: any, res: any) => {
     try {
         const xpriv = _req.body.xpriv;
         const chain = _req.body.chain;
         console.log(chain);
-        
-        const response = await generateAddressKeypair(xpriv,0,118)
+
+        const response = await generateAddressKeypair(xpriv, 0, 118)
         res.status(200).send(response)
 
     } catch (error) {
@@ -82,10 +111,10 @@ const ChildKeypair = async(_req: any, res: any) =>{
     }
 }
 
-const FluxId = async(_req: any, res: any) =>{
+const FluxId = async (_req: any, res: any) => {
     try {
         const xpriv = _req.body.xpriv;
-        const response = await generateExternalIdentityKeypair(xpriv)
+        const response = await generateExternalIdentityKeypair(xpriv);
         res.status(200).send(response)
 
     } catch (error) {
@@ -116,11 +145,12 @@ const FluxId = async(_req: any, res: any) =>{
 // }
 
 
-export{
+export {
     // createPrivKey,
     // LogIn,
     // auth,
     ChildKeypair,
+    createGridAccount,
     fluxTest,
     FluxId,
     XprivXpub,
--- src/routes/operatorRoutes.ts
+++ src/routes/operatorRoutes.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { fluxTest, FluxId, ChildKeypair, XprivXpub, akashAccount} from "../controllers/operatorController";
+import { fluxTest, FluxId, ChildKeypair, XprivXpub, akashAccount, createGridAccount} from "../controllers/operatorController";
 
 
 const router = express.Router()
 
 router.post("/createFlux", fluxTest);
+router.post("/gridAccount", createGridAccount);
 router.post("/createFluxId", FluxId);
 // router.post("/getZelId", auth);
 router.post("/child", ChildKeypair);
